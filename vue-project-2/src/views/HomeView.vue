55a9ac714d154b31ebfbff0d4ffc01a62bd25363
liuchanglei <liuchanglei@MacBook-Pro.local>
2023-12-19 19:21:46 +0800

    初始化项目，设置1)vue3 2)vue-router 3)element-plus; 新增Home主页、register页

---
+++ vue-project-2/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="4">
+            <div class="block">
+              <el-avatar :size="40" src="../assets/银魂头像1.png"> user </el-avatar>
+            </div>
+          </el-col> 
+          <el-col :span="6" :offset="14">
+            <RouterLink to="/Login" style="">
+              <el-button type="primary">
+                登陆
+              </el-button>
+            </RouterLink>
+            <RouterLink to="/Register" style="">
+              <el-button>
+                注册
+              </el-button>
+            </RouterLink>
+          </el-col>
+        </el-row>
+      </el-header>
+      <div style="width: 100%; height: 0; border-top: 1px solid var(--el-border-color);"/>
+      <el-main>
+        <el-row class="row-bg" justify="center">
+          <el-col :span="12">
+            <el-text class="mx-1" type="info">
+              欢迎来到我的博客！更多功能正在开发中，敬请期待～😄
+            </el-text>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+</template>
---
+++ vue-project-2/src/views/RegisterView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+    import { reactive } from 'vue'
+
+    // do not use same name with ref
+    const form = reactive({
+        email: '',
+        password: ''
+    })
+
+    const onSubmit = () => {
+    console.log('submit!')
+}
+</script>
+
+<template>
+  <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="4">
+            <div class="block">
+              <el-avatar :size="40" src="../assets/银魂头像1.png"> user </el-avatar>
+            </div>
+          </el-col> 
+        </el-row>
+      </el-header>
+      <div style="width: 100%; height: 0; border-top: 1px solid var(--el-border-color);"/>
+      <el-main>
+        <el-row class="row-bg" justify="center">
+            <el-form :model="form" label-width="120px">
+                <el-form-item label="邮箱">
+                    <el-input v-model="form.email" />
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="form.password" />
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">提交</el-button>
+                    <RouterLink to="/" style="">
+                        <el-button>
+                            取消
+                        </el-button>
+                    </RouterLink>
+                </el-form-item>
+            </el-form>
+        </el-row>
+      </el-main>
+    </el-container>
+</template>
